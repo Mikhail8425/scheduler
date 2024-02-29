2043436455b96e72aa5768f3b998bebdba71a14e
Mikhail8425 <mikhailshemet@gmail.com>
2024-02-28 21:56:52 -0500

    all works

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,8 +1,15 @@
 import { useState } from "react";
+
 export default function useVisualMode(initial) {
+
+  console.log('useVisualMode called');
+  console.log('arg: initial', initial);
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
+
+  //transition from mode to mode
   function transition(nextMode, replace = false) {
+    console.log('transition called');
     if(replace){
       setHistory(prev => prev.slice(0, -1));
       setHistory(prev => [...prev, nextMode]);
@@ -11,13 +18,14 @@ export default function useVisualMode(initial) {
       }
       setMode(nextMode);
   }
-  //back function
+  //going back to previous mode
   function back() {
+    console.log('back called');
     if(history.length > 1) {
       setHistory(history.slice(0, -1));
       setMode(history[history.length-2]);
     }
   }
 
-  return { mode, transition, back};
+  return { mode, transition, back };
 }
